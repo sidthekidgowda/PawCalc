5d58e9baf90da84d693c3f3ba237e509bb7f4ddd
Sid <sidgowda.9@gmail.com>
2023-04-06 22:37:11 -0700

    removed suppress violation

---
+++ feature/newdog/src/test/java/com/sidgowda/pawcalc/newdog/NewDogViewModelTest.kt
@@ -0,0 +1,30 @@
+package com.sidgowda.pawcalc.newdog
+
+import org.junit.Before
+import org.junit.Test
+
+class NewDogViewModelTest {
+
+
+    @Before
+    fun setup() {
+
+    }
+
+    @Test
+    fun `test this`() {
+
+    }
+
+    // test profile pic
+    // test name
+    // test weight
+    // test birth date
+    // test save button
+    // test name requirements, error
+    // test weight requirements, error
+    // test birth date requirements, error
+    // test valid to invalid state
+    // test invalid to valid state
+}
+
